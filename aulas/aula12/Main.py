f6839afd2d2810c39b9121ba5fb951398c9dd09a
Luislima07 <luis.lima133@etec.sp.gov.br>
2024-11-26 16:54:52 -0300

    Aula 12 - Exercíco continuado

--- aulas/aula12/Main.py
+++ aulas/aula12/Main.py
@@ -1,12 +1,15 @@
 from Pessoa import *
 import os
-def limpar(): os.system('clear')
+def limpar():
+    os.system('clear')
 
 limpar()
+
+
+
 def pergunta():
     res = int(input("Deseja cadastrar uma nova pessoa? 1 - Sim ou 0 - Não: "))
     return res
-
 
 cadastro = []
 
@@ -20,40 +23,64 @@ while(res == 1):
     cadastro.append(Pessoa(nome,idade,cargo,salario))
     res = pergunta()
 
-y = 1
 def mostrar():
+    y = 0
     print("{:<4} {:<10} {:<7} {:<10} {:<7}"
           .format("N°","Nome","Idade","Cargo","Salário") )
     for x in cadastro:
         print("{:<4} {:<10} {:<7} {:<10} {:<7}"
-              .format(y +1,
+              .format(y,
                       x.get_nome(),
                       x.get_idade(),
                       x.get_cargo(),
                       x.get_salario(),
                       ))
-mostrar()
+        y += 1
+        mostrar()
+
+
 
 def questionar():
     confirma = int(input("Deseja alterar a lista? 1 - Sim ou 0 - Não: "))
     return confirma
 
-confirma = questionar() 
+confirma = questionar()
 
 while(confirma == 1):
-    def alterar(x, opcao, valor):
-        x = int(input("Digite o N° do funcionário: "))
-        opcao = str(input("Digite a opção que deseja alterar: "))
-        valor = str(input("Escreva o novo valor da opção: "))
-        if(opcao == 0): cadastro[x].set_nome(valor)
-        if(opcao == 0): cadastro[x].set_idade(valor)
-        if(opcao == 0): cadastro[x].set_cargo(valor)
-        if(opcao == 0): cadastro[x].set_salario(valor)
-mostrar()
+        def alterar():
+            x = int(input("Digite o N° do funcionário: "))
+            opcao = int(input("Digite a opção que deseja alterar: \n1 - Nome \n2 - Idade \n3 - Cargo \n4 - Salário \n"))
+            valor = str(input("Escreva o novo valor da opção: "))
+            if(opcao == 1): cadastro[x].set_nome(valor)
+            elif(opcao == 2): cadastro[x].set_idade(valor)
+            elif(opcao == 3): cadastro[x].set_cargo(valor)
+            elif(opcao == 4): cadastro[x].set_salario(valor)
+            else:
+                 print("Valor incorreto!")
+            alterar()
+            mostrar()
 
+def excluir():
+     apagar = int(input("Deseja excluir um funcionário da lista? \n1 - Sim \n2- Não\n"))
+     return apagar
 
+apagar = excluir()
 
-        
+while(apagar == 1):
+        def remover():
+            x = int(input("Digite o número do funcionário que deseja remover: "))
+            res = int(input("Tem certeza que deseja remover o funcionário número?\n1 - Sim \n2 - Não\n"))
+            if (res == 1):
+                cadastro.pop(x)
+                remover()
+                mostrar()
 
-    
-    
+def menu():
+        x = int(input(("Escolha a ação que deseja fazer: \n1 - Cadastrar funcionário \n2 - Alterar tabela de funcionários \n3 - Remover funcionário \n4 - Mostrar tabela\n")))
+        if(x == 1): pergunta()
+        elif(x == 2): questionar()
+        elif(x == 3): remover()
+        elif(x == 4): mostrar()
+        else:
+            print("Número inválido! Sessão encerrada")
+menu()
